dd9f2a1e992e4835c7bb088d85a38f0bc66f61bd
Jarvis <jarvis@jarvis-macmini.taild97556.ts.net>
2026-09-03 21:47:16 +0200

    test: accept Hermes action cancel helper

--- scripts/verify-hermes-release.sh
+++ scripts/verify-hermes-release.sh
@@ -57,6 +57,7 @@ expected = {
     "reman_accounting_read",
     "reman_accounting_prepare_action",
     "reman_accounting_action",
+    "reman_agentic_action_cancel",
     "reman_accounting_prepare_file_action",
     "reman_accounting_list_companies",
     "reman_accounting_search_partners",
